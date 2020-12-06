238d96ec943fc3028b412a009abb5cb6757bd571
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2020-12-06 10:13:17 +0100

    day6

---
+++ 2020/6/day6.nim
@@ -0,0 +1,28 @@
+import sets, tables, strutils
+
+type
+  Answer = 'a' .. 'z'
+
+proc part1(): int =
+  let data = readFile("day6.txt")
+  for group in data.split("\n\n"):
+    var answers: HashSet[Answer]
+    for line in group.splitLines:
+      for c in line:
+        answers.incl c
+    result += answers.len
+
+proc part2(): int =
+  let data = readFile("day6.txt")
+  for group in data.split("\n\n"):
+    var answers: CountTable[Answer]
+    for line in group.splitLines:
+      for c in line:
+        answers.inc c
+    for (keys, count) in answers.pairs:
+      if count == group.countLines:
+        result += 1
+
+when isMainModule:
+  echo "Part1: ", part1()
+  echo "Part2: ", part2()
