bf06509836456fd7c3f60e5dddce8551e15f17d3
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2020-12-15 11:56:47 +0100

    speedup: use seq instead of table

--- 2020/15/day15.nim
+++ 2020/15/day15.nim
@@ -1,8 +1,8 @@
-import tables
+import tables, benchy
 
 proc calcDay15(limit: int): int =
   var turn = 1
-  var counter: Table[int, (int, int)] # int -> (last, 2nd last)
+  var counter = newSeq[(int, int)](limit) # int -> (last, 2nd last)
   let starting = [0,3,1,6,7,5]
   var last: int
   for i in starting:
@@ -11,12 +11,12 @@ proc calcDay15(limit: int): int =
     turn += 1
   while turn <= limit:
     if counter[last][1] == 0:
-      let lastTurn = counter.getOrDefault(0)[0]
+      let lastTurn = counter[0][0]
       counter[0] = (turn, lastTurn)
       last = 0
     else:
       let diff = counter[last][0] - counter[last][1]
-      let lastTurn = counter.getOrDefault(diff)[0]
+      let lastTurn = counter[diff][0]
       counter[diff] = (turn, lastTurn)
       last = diff
     turn += 1
@@ -25,3 +25,8 @@ proc calcDay15(limit: int): int =
 when isMainModule:
   echo "Part 1: ", calcDay15(2020)
   echo "Part 2: ", calcDay15(30000000)
+  timeit "Part 1":
+    keep calcDay15(2020)
+  timeit "Part 2":
+    keep calcDay15(30000000)
+  
