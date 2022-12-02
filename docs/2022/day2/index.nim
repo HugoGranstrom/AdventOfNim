611a39d74c689698c31b9914682e090c9a1ed8d0
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-12-02 15:45:21 +0100

    add text and finetune animations

--- docs/2022/day2/index.nim
+++ docs/2022/day2/index.nim
@@ -3,6 +3,8 @@ import nimib, p5
 nbInit
 nbUseP5
 
+nbText: "# Advent of Code 2022 - Day 2 - Rock Paper Scissors"
+
 let input = readFile("input.txt")
 
 nbJsFromCode(input):
@@ -44,8 +46,6 @@ nbJsFromCode(input):
   var matches: seq[Match]
   for i in 0 .. hands.left.high:
     matches.add Match(left: hands.left[i], right: hands.right[i], start_t: if i < 200: i*0.015 else: i*0.01)
-
-  echo matches
   
   let n_matches = matches.len # 2500 -> 50x50
   let match_grid = 50
@@ -58,8 +58,6 @@ nbJsFromCode(input):
   let height = 300
   let eachHeight = height div match_grid
   let eachWidth = width div (match_grid)
-  let excessWidth = (width mod (match_grid)) / match_grid
-  echo excessWidth
 
   let emojiTable = {Rock: "✊", Paper: "🤚", Scissor: "✌️"}.toTable
   let leftHandEmoji = "🤜"
@@ -69,30 +67,31 @@ nbJsFromCode(input):
   let drawEmoji = "➖"
 
   proc drawMatch(m: Match) =
+    ## Draws a single match
     push()
-    #translate(-(tSize div 2), -(tSize div 2))
-    let x1 = 0#-(eachWidth div 2)
-    let y1 = 0#-(tSize div 2)
+    let x1 = 0
+    let y1 = 0
     let x2 = x1 + eachHeight
     let y2 = y1
 
     let localTime = t - m.start_t
 
     if localTime < 0:
+      # Before match has started
       text(leftHandEmoji, x1, y1)
       text(rightHandEmoji, x2, y2)
     elif localTime < matchLength:
-      #point(0,0)
+      # Match has started, shake hands
       rotate(0.5*(sin(w*localTime) - 0.3)):
         text(leftHandEmoji, x1, y1)
       translate(eachHeight, 0):
-        #point(0,0)
-        rotate(0.5*(sin(w*localTime) - 0.3)):
+        rotate(-0.5*(sin(w*localTime) - 0.3)):
           text(rightHandEmoji, x1, y1)
     elif localTime in matchLength .. matchLength + 2.0:
+      # Match has ended, show result
       text(emojiTable[m.left], x1, y1)
       text(emojiTable[m.right], x2, y2)
-    else: # t - m.start_t in matchLength + 1.0 .. matchLength + 2.0:
+    else:
       if winTable[m.right] == m.left:
         text(winEmoji, x1, y1)
       elif loseTable[m.right] == m.left:
@@ -111,14 +110,15 @@ nbJsFromCode(input):
   let scaleTime = 20.0
   draw:
     background(200)
-    echo scaleFactor
     scale(scaleFactor)
+    # First scale fast and then slower
     if t < scaleTime / 3:
       scaleFactor = lerp(20, 3, t/(scaleTime/3))
     elif t in scaleTime / 3 .. scaleTime:
       scaleFactor = lerp(3, 1, (t - scaleTime/3) / (2/3*scaleTime))
     else:
       scaleFactor = 1
+    # Draw every match
     for i in 0 ..< min(int(match_grid / scaleFactor) + 1, matchGrid):
       translate(0, (i+1)*eachHeight):
           for j in 0 ..< min(int(match_grid / scaleFactor) + 1, matchGrid): 
@@ -127,31 +127,19 @@ nbJsFromCode(input):
               let m = matches[index]
               #m.start_t = index * 0.1
               m.drawMatch()
-        
-    #translate(100, 100)
-    #match.drawMatch()
-    #[ push()
-    let x = 100
-    let y = 100 #+ 10*sin(10*t + 0.1)
-    translate(x, y)
-    if t < 3*period:
-      rotate(-0.5*(sin(w*t+0.1) + 0.3))
-      text(handEmoji, -16, -16)
-    else:
-      text(emojiTable[match[0]], -16, -16)
-    pop()
-    push()
-    translate(200, 100)
-    if t < 3*period:
-      rotate(0.5*(sin(10*t+0.1) - 0.3))
-      text(handEmoji, -16, -16)
-    else:
-      text(emojiTable[match[1]], -16, -16)
-    pop() ]#
+
     t += dt
   
   keyPressed:
     if key == "s":
-      saveGif("day1", 30)
+      saveGif("day2", 30)
+
+nbText: hlMd"""
+The input for today contained 2500 games of Rock paper scissor. The above animation shows all of those games visualized
+using [p5nim](https://github.com/pietroppeter/p5nim), a wrapper of P5.js for Nim. It starts off with both players getting
+ready to play (hands are still), then they start shaking their hands and reveal their choice. The left one is the elfs choice
+and the right one is your choice. After the game an emoji will show the result of the game, a green checkbox means you won that
+match, a dash means it was a draw and a red cross means you lost.  
+"""
 
 nbSave
