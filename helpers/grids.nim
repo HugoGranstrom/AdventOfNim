27c4b599b1d037f14c9b7390de55de1db34a2f23
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2025-11-27 21:25:47 +0100

    implement SparseGrid

--- helpers/grids.nim
+++ helpers/grids.nim
@@ -10,6 +10,7 @@ type
 
   SparseGrid*[T] = ref object
     data*: Table[GridCoordinate, T]
+    origo*: GridCoordinate = (0, 0)
 
   NeighboursMode* = enum
     Cross
@@ -45,7 +46,7 @@ proc `$`*[T](grid: Grid[T]): string =
 proc initGrid*[T](width, height: int, origo: GridCoordinate = (0, 0)): Grid[T] =
   Grid[T](data: newTensor[T](width, height), origo: origo)
 
-func normalizeCoord*(grid: Grid, coord: GridCoordinate): GridCoordinate =
+func normalizeCoord*(grid: Grid or SparseGrid, coord: GridCoordinate): GridCoordinate =
   (x: coord.x + grid.origo.x, y: coord.y + grid.origo.y)
 
 func containsCoordinate*(grid: Grid, coord: GridCoordinate): bool =
@@ -83,25 +84,88 @@ iterator neighbours*[T](grid: var Grid[T], coord: GridCoordinate, mode: Neighbou
       if grid.containsCoordinate(c):
         yield (c, grid[c])
 
+### SparseGrid
+
+proc `$`*[T](grid: SparseGrid[T]): string =
+  $(grid[].data)
+
+proc initSparseGrid*[T](origo: GridCoordinate = (0, 0)): SparseGrid[T] =
+  SparseGrid[T](origo: origo)
+
+func containsCoordinate*(grid: SparseGrid, coord: GridCoordinate): bool =
+  let coord = grid.normalizeCoord(coord)
+  coord in grid.data
+
+proc `[]`*[T](grid: SparseGrid[T], coord: GridCoordinate): T =
+  let coord = grid.normalizeCoord(coord)
+  grid.data[coord]
+
+proc `[]=`*[T](grid: var SparseGrid[T], coord: GridCoordinate, val: T) =
+  let coord = grid.normalizeCoord(coord)
+  grid.data[coord] = val
+
+proc parseCharSparseGrid*[T](s: string, parseFunc: (c: char) -> T = (c: char) => c): SparseGrid[T] =
+  let lines = s.splitLines
+  let height = lines.len
+  let width = lines[0].len
+  result = initSparseGrid[T]()
+  for x in 0 ..< width:
+    for y in 0 ..< height:
+      result[(x, y)] = parseFunc(lines[y][x])
+
+iterator neighbours*[T](grid: var SparseGrid[T], coord: GridCoordinate, mode: NeighboursMode): tuple[coord: GridCoordinate, value: var T] =
+  if mode in [Diag, All]:
+    for xOffset in [-1, 1]:
+      for yOffset in [-1, 1]:
+        let c = (x: coord.x + xOffset, y: coord.y + yOffset)
+        if grid.containsCoordinate(c):
+          yield (c, grid[c])
+  
+  if mode in [Cross, All]:
+    for (xOffset, yOffset) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+      let c = (x: coord.x + xOffset, y: coord.y + yOffset)
+      if grid.containsCoordinate(c):
+        yield (c, grid[c])
 
 if isMainModule:
-  var grid = initGrid[bool](10, 11)
+  if false:
+    var grid = initGrid[bool](10, 11)
 
-  grid[(9, 10)] = true
+    grid[(9, 10)] = true
 
-  for (c, v) in grid.neighbours((5, 6), All):
-    echo (c, v)
+    for (c, v) in grid.neighbours((5, 6), All):
+      echo (c, v)
 
-  echo grid
+    echo grid
 
-  let gridStr = """
-# # # #
-#######
-      #
-""".strip
+    let gridStr = """
+  # # # #
+  #######
+        #
+  """.strip
 
-  grid = parseCharGrid[bool](gridStr, (c: char) => not c.isSpaceAscii)
-  echo grid
+    grid = parseCharGrid[bool](gridStr, (c: char) => not c.isSpaceAscii)
+    echo grid
+
+  if true:
+    var sparseGrid = initSparseGrid[bool]((1,1))
+
+    sparseGrid[(5, 5)] = true
+
+    for (c, v) in sparseGrid.neighbours((6, 6), All):
+      echo (c, v)
+
+    echo sparseGrid
+
+    let gridStr = """
+  # # # #
+  #######
+        #
+  """.strip
+
+    sparseGrid = parseCharSparseGrid[bool](gridStr, (c: char) => not c.isSpaceAscii)
+
+    echo sparseGrid
 
 
 
