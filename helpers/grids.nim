17ff8328df57ec2f40006b7cce3a28437649cfff
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2025-11-27 23:08:33 +0100

    change grid backend to seq of seq

--- helpers/grids.nim
+++ helpers/grids.nim
@@ -1,11 +1,10 @@
-import std / [sequtils, tables, sugar, strutils]
-import arraymancer
+import std / [sequtils, tables, sugar, strutils, math]
 
 type
   GridCoordinate* = tuple[x: int, y: int]
 
   Grid*[T] = ref object
-    data*: Tensor[T]
+    data*: seq[seq[T]]
     origo*: GridCoordinate = (0, 0)
 
   SparseGrid*[T] = ref object
@@ -41,25 +40,28 @@ defineGridCoordinateOp(`*`)
 ### Grid
 
 proc `$`*[T](grid: Grid[T]): string =
-  $(grid[].data.transpose)
+  $(grid[].data)
 
 proc initGrid*[T](width, height: int, origo: GridCoordinate = (0, 0)): Grid[T] =
-  Grid[T](data: newTensor[T](width, height), origo: origo)
+  Grid[T](data: newSeqWith(height, newSeq[T](width)), origo: origo)
 
 func normalizeCoord*(grid: Grid or SparseGrid, coord: GridCoordinate): GridCoordinate =
-  (x: coord.x + grid.origo.x, y: coord.y + grid.origo.y)
+  coord + grid.origo
+
+func denormalizeCoord*(grid: Grid or SparseGrid, coord: GridCoordinate): GridCoordinate =
+  coord - grid.origo
 
 func containsCoordinate*(grid: Grid, coord: GridCoordinate): bool =
   let coord = grid.normalizeCoord(coord)
-  coord.x >= 0 and coord.y >= 0 and coord.x < grid.data.shape[0] and coord.y < grid.data.shape[1]
+  coord.x >= 0 and coord.y >= 0 and coord.x < grid.data[0].len and coord.y < grid.data.len
 
 proc `[]`*[T](grid: Grid[T], coord: GridCoordinate): T =
   let coord = grid.normalizeCoord(coord)
-  grid.data[coord.x, coord.y]
+  grid.data[coord.y][coord.x]
 
 proc `[]=`*[T](grid: var Grid[T], coord: GridCoordinate, val: T) =
   let coord = grid.normalizeCoord(coord)
-  grid.data[coord.x, coord.y] = val
+  grid.data[coord.y][coord.x] = val
 
 proc parseCharGrid*[T](s: string, parseFunc: (c: char) -> T = (c: char) => c): Grid[T] =
   let lines = s.splitLines
@@ -70,7 +72,7 @@ proc parseCharGrid*[T](s: string, parseFunc: (c: char) -> T = (c: char) => c): G
     for y in 0 ..< height:
       result[(x, y)] = parseFunc(lines[y][x])
 
-iterator neighbours*[T](grid: var Grid[T], coord: GridCoordinate, mode: NeighboursMode): tuple[coord: GridCoordinate, value: var T] =
+iterator neighbours*[T](grid: Grid[T], coord: GridCoordinate, mode: NeighboursMode): tuple[coord: GridCoordinate, value: T] =
   if mode in [Diag, All]:
     for xOffset in [-1, 1]:
       for yOffset in [-1, 1]:
@@ -83,6 +85,11 @@ iterator neighbours*[T](grid: var Grid[T], coord: GridCoordinate, mode: Neighbou
       let c = (x: coord.x + xOffset, y: coord.y + yOffset)
       if grid.containsCoordinate(c):
         yield (c, grid[c])
+
+iterator items*[T](grid: Grid[T]): GridCoordinate =
+  for y in 0 .. grid.data.high:
+    for x in 0 .. grid.data[0].high:
+      yield grid.denormalizeCoord((x, y))
 
 ### SparseGrid
 
@@ -104,7 +111,7 @@ proc `[]=`*[T](grid: var SparseGrid[T], coord: GridCoordinate, val: T) =
   let coord = grid.normalizeCoord(coord)
   grid.data[coord] = val
 
-proc parseCharSparseGrid*[T](s: string, parseFunc: (c: char) -> T = (c: char) => c): SparseGrid[T] =
+proc parseCharSparseGrid*[T](s: string, parseFunc: (c: char) -> T): SparseGrid[T] =
   let lines = s.splitLines
   let height = lines.len
   let width = lines[0].len
@@ -128,10 +135,10 @@ iterator neighbours*[T](grid: var SparseGrid[T], coord: GridCoordinate, mode: Ne
         yield (c, grid[c])
 
 if isMainModule:
-  if false:
-    var grid = initGrid[bool](10, 11)
+  if true:
+    var grid = initGrid[char](10, 11)
 
-    grid[(9, 10)] = true
+    grid[(9, 10)] = 'x'
 
     for (c, v) in grid.neighbours((5, 6), All):
       echo (c, v)
@@ -144,10 +151,10 @@ if isMainModule:
         #
   """.strip
 
-    grid = parseCharGrid[bool](gridStr, (c: char) => not c.isSpaceAscii)
+    grid = parseCharGrid[char](gridStr, (c: char) => c)
     echo grid
 
-  if true:
+  if false:
     var sparseGrid = initSparseGrid[bool]((1,1))
 
     sparseGrid[(5, 5)] = true
