710d3ae022fbadf3c1b828c1ac48cbc266669244
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2025-11-28 19:56:51 +0100

    keep working on graphs

--- helpers/graphs.nim
+++ helpers/graphs.nim
@@ -12,7 +12,14 @@ type
     coord*: GridCoordinate
 
   Graph*[T] = ref object
-    nodes*: seq[GraphNode[T]]
+    nodes*: HashSet[GraphNode[T]]
+
+  Path*[T] = ref object
+    steps*: seq[GraphNode[T]]
+    # cost?
+
+proc `$`*[T](path: Path[T]): string =
+  $path[]
 
 proc `$`*[T](node: GraphNode[T]): string =
   let connections = collect(newSeq):
@@ -32,10 +39,27 @@ proc initGraph*[T](): Graph[T] =
   Graph[T]()
 
 proc add*[T](graph: var Graph[T], node: GraphNode[T], bidirectional: bool = false) =
-  graph.nodes.add node
+  graph.nodes.incl node
   if bidirectional:
     for neighbour in node.links:
       neighbour.links.incl node
+
+proc fromTable*[T](t: Table[T, seq[T]], bidirectional=false): Graph[T] =
+  result = initGraph[T]()
+  var allKeys = t.keys.toSeq.toHashSet
+  for (key, values) in t.pairs:
+    for v in values:
+      allKeys.incl v
+  var nodes = collect(initTable()):
+    for key in allKeys:
+      {key: GraphNode[T](value: key)}
+  
+  for key in allKeys:
+    var node = nodes[key]
+    if key in t:
+      for neigh in t[key]:
+        node.links.incl nodes[neigh]
+    result.add(node, bidirectional)
 
 proc fromGrid*[T, Y](grid: Grid[T], mode: NeighboursMode, isConnectedFunc: (grid: Grid[T], center: GridCoordinate, neighbour: GridCoordinate) -> bool, valueFunc: (grid: Grid[T], coord: GridCoordinate) -> Y): Graph[Y] =
   result = initGraph[Y]()
@@ -52,8 +76,57 @@ proc fromGrid*[T, Y](grid: Grid[T], mode: NeighboursMode, isConnectedFunc: (grid
       node.value = valueFunc(grid, coord)
       result.add(node)
 
+template findNodeWith*[T](graph: Graph[T], op: untyped): GraphNode[T] =
+  var result: GraphNode[T]
+  for node in graph.nodes:
+    let it {.inject.} = node
+    if op:
+      result = node
+      break
+  result
+
+template findNodesWith*[T](graph: Graph[T], op: untyped): seq[GraphNode[T]] =
+  var result: seq[GraphNode[T]]
+  for node in graph.nodes:
+    let it {.inject.} = node
+    if op:
+      result.add node
+  result
+
+
+# proc firstPop[T](l: var seq[T]): T =
+#   assert l.len > 0
+#   result = l[0]
+#   l.delete(0)
+
+# # will need a new datatype for search where we can keep the score. Or another datastructure
+# proc dijkstra(start, goal: Graph): int =
+#   var queue: seq[Graph] = @[start]
+#   while queue.len > 0:
+#     let current = queue.firstPop()
+#     for n in current.neighbors:
+#       if n.score == -1 or n.score > current.score + 1:
+#         n.score = current.score + 1
+#         queue.insert(n, upperBound(queue, n))
+#   result = goal.score
+
+
+# How do we specify the cost function? Take two (neighbouring) nodes as input and output a value?
+# In the simplest case it always returns 1 to keep 
+
+proc internalDepthSearch*[T]() =
+  discard
+
+proc defaultStepCostFunction*[T](src: GraphNode[T], dest: GraphNode[T]): float =
+  1
+
+proc depthPath*[T](graph: Graph[T], startNode, endNode: GraphNode[T], stepCostFunction: proc(src: GraphNode[T], dest: GraphNode[T]): float {.nimcall.} = defaultStepCostFunction[T]): Path[T] =
+
+  echo stepCostFunction(startNode, endNode)
+  Path[T]()
+
 # some way of iterating through all nodes
-# djikstraPath(startPos, endPos): seq[GraphNode]
+# djikstraPath(startPos: GraphNode, endPos: GraphNode): seq[GraphNode]
 # depthPath
 # breadthPath
 
@@ -64,13 +137,30 @@ proc exampleValueFunc*(grid: Grid[char], coord: GridCoordinate): GridValue[char]
   GridValue[char](value: grid[coord], coord: coord)
 
 if isMainModule:
-  # y is only linked to (3, 2), but not (4, 1)
-  let grid = parseCharGrid[char]("""
+  # y (3, 1) is only linked to (3, 2), but not (4, 1)
+  if true:
+    let grid = parseCharGrid[char]("""
 #### ##
 #x#y   
 #   #a#
 #######
 """.strip, (c: char) => c)
-  
-  var graph = fromGrid(grid, Cross, exampleIsConnected, exampleValueFunc)
-  echo $graph
+    
+    var graph = fromGrid(grid, Cross, exampleIsConnected, exampleValueFunc)
+    echo $graph
+    echo "----------------"
+    let startNode = graph.findNodeWith(it.value.value == 'x')
+    let endNode = graph.findNodeWith(it.value.value == 'a')
+    echo startNode, " ", endNode
+
+    let path = depthPath(graph, startNode, endNode)
+    echo path
+
+  if false:
+    let graph = fromTable({
+      1: @[2],
+      2: @[3],
+      3: @[2, 4],
+    }.toTable, true)
+
+    echo graph
